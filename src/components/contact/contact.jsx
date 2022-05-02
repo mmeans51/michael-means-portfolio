282b13d2b137bee36a28af317841e2110c367c6a
michael Means <colemeans51@gmail.com>
2022-05-02 18:29:51 -0500

    save

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,13 +1,13 @@
 import "./contact.css";
-import Phone from "../../img/phone.png";
-import Email from "../../img/email.png";
-import Address from "../../img/address.png";
+import phone from "../../img/phone.png"
+import email from "../../img/email.png"
+import address from "../../img/address.png"
 import { useContext, useRef, useState } from "react";
 import emailjs from "emailjs-com";
 import { ThemeContext } from "../../context";
 
-const Contact = () => {
-  const form = useRef();
+const Contactme = () => {
+  const formRef = useRef();
   const [done, setDone] = useState(false)
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
@@ -16,10 +16,10 @@ const Contact = () => {
     e.preventDefault();
     emailjs
       .sendForm(
-        "service_xpm51v6",
-        "template_c6b4lpl",
-        form.current,
-        "tFAJmHj4bU_mGSiHs"
+        "service_rrvnzco",
+        "template_3v5nih4",
+        formRef.current,
+        "user_DrriDPTGKO2Zj4RDXCA6W"
       )
       .then(
         (result) => {
@@ -31,34 +31,32 @@ const Contact = () => {
         }
       );
   };
-
   return (
-    <div className="c">
-      <div className="c-bg"></div>
-      <div className="c-wrapper">
-        <div className="c-left">
-          <h1 className="c-title">Let's discuss your project</h1>
-          <div className="c-info">
-            <div className="c-info-item">
-              <img src={Phone} alt="" className="c-icon" />
-              +1 903 808 0914
+    <div className="contact">
+      <div className="bg"></div>
+      <div className="wrapper">
+        <div className="left">
+          <h1 className="title">My Contacts</h1>
+          <div className="info">
+            <div className="item">
+              <img src={phone} alt="" className="icon" />
+              +1 (903) 808 0914
             </div>
-            <div className="c-info-item">
-              <img className="c-icon" src={Email} alt="" />
+            <div className="item">
+              <img className="icon" src={email} alt="" />
               colemeans51@gmail.com
             </div>
-            <div className="c-info-item">
-              <img className="c-icon" src={Address} alt="" />
-              6061 Village Bend, Dallas, Tx 75206
+            <div className="item">
+              <img className="icon" src={address} alt="" />
+              my address
             </div>
           </div>
         </div>
-        <div className="c-right">
-          <p className="c-desc">
-            <b>What’s your story?</b> Get in touch. Always available for
-            freelancing if the right project comes along. me.
-          </p>
-          <form ref={form} onSubmit={handleSubmit}>
+        <div className="right">
+            <p className="description">
+               <b>lets chat!</b> enter your info and a brief description of projects, job offers, or to schedule a phone or zoom call.
+            </p>
+            <form ref={formRef} onSubmit={handleSubmit}>
             <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
             <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
             <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
@@ -69,7 +67,8 @@ const Contact = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
-export default Contact;
+export default Contactme;
